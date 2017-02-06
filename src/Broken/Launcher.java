df4a3dd652dc1f8139e194f7fae65a38c8da4ece
BrokenFire <seb6596@gmail.com>
2017-02-07 00:41:34 +0100

    First launch test

---
+++ src/Broken/Launcher.java
@@ -0,0 +1,71 @@
+package Broken;
+
+import fr.theshark34.openauth.AuthPoints;
+import fr.theshark34.openauth.AuthenticationException;
+import fr.theshark34.openauth.Authenticator;
+import fr.theshark34.openauth.model.AuthAgent;
+import fr.theshark34.openauth.model.response.AuthResponse;
+import fr.theshark34.openlauncherlib.LaunchException;
+import fr.theshark34.openlauncherlib.internal.InternalLaunchProfile;
+import fr.theshark34.openlauncherlib.internal.InternalLauncher;
+import fr.theshark34.openlauncherlib.minecraft.*;
+import fr.theshark34.supdate.BarAPI;
+import fr.theshark34.supdate.SUpdate;
+import fr.theshark34.supdate.exception.BadServerResponseException;
+import fr.theshark34.supdate.exception.BadServerVersionException;
+import fr.theshark34.supdate.exception.ServerDisabledException;
+import fr.theshark34.supdate.exception.ServerMissingSomethingException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by seb65 on 06/02/2017.
+ */
+public class Launcher {
+    public static final GameVersion MC_VERSION = new GameVersion("1.7.10", GameType.V1_7_10);
+    public static final GameInfos MC_INFOS = new GameInfos("Imerir",MC_VERSION,new GameTweak[]{});
+    public static final File MC_DIR = MC_INFOS.getGameDir();
+    private static AuthInfos authInfos;
+
+    public static void auth(String user, String password) throws AuthenticationException {
+        Authenticator authenticator = new Authenticator(Authenticator.MOJANG_AUTH_URL, AuthPoints.NORMAL_AUTH_POINTS);
+        AuthResponse authResponse = authenticator.authenticate(AuthAgent.MINECRAFT,user,password,"");
+        authInfos = new AuthInfos(authResponse.getSelectedProfile().getName(),authResponse.getClientToken(),authResponse.getSelectedProfile().getId());
+
+    }
+
+    public static void update() throws BadServerResponseException, IOException, BadServerVersionException, ServerDisabledException, ServerMissingSomethingException {
+        SUpdate su = new SUpdate("http://192.168.1.9/",MC_DIR);
+        su.getServerRequester().setRewriteEnabled(true);
+
+        Thread thread = new Thread(){
+            long value = BarAPI.getNumberOfTotalDownloadedBytes()/1000;
+            long max = BarAPI.getNumberOfTotalBytesToDownload()/1000;
+
+            @Override
+            public void run() {
+                while(!this.isInterrupted())
+                {
+                    max = BarAPI.getNumberOfTotalBytesToDownload()/1000;
+                    if(value != BarAPI.getNumberOfTotalDownloadedBytes()/1000)
+                    {
+                        value = BarAPI.getNumberOfTotalDownloadedBytes()/1000;
+                        float pour = (value*100)/max;
+                        System.out.println(value+"/"+max+"   -> "+pour);
+                    }
+
+                }
+            }
+        };
+        thread.start();
+        su.start();
+        thread.interrupt();
+    }
+
+    public static void lauch() throws LaunchException {
+        InternalLaunchProfile profile = MinecraftLauncher.createInternalProfile(MC_INFOS,GameFolder.BASIC,authInfos);
+        InternalLauncher launcher = new InternalLauncher(profile);
+        launcher.launch();
+    }
+}
--- src/Broken/Main.java
+++ src/Broken/Main.java
@@ -1,10 +1,19 @@
 package Broken;
 
+import fr.theshark34.openauth.AuthenticationException;
+import fr.theshark34.openlauncherlib.LaunchException;
+import fr.theshark34.supdate.exception.BadServerResponseException;
+import fr.theshark34.supdate.exception.BadServerVersionException;
+import fr.theshark34.supdate.exception.ServerDisabledException;
+import fr.theshark34.supdate.exception.ServerMissingSomethingException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -14,10 +23,30 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        try {
+            Launcher.auth("thegoodnoob65@gmail.com","seb6540096");
+            Launcher.update();
+            Launcher.lauch();
+        } catch (AuthenticationException e) {
+            System.out.println("Erreur d'authentification: "+e.getErrorModel().getErrorMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BadServerVersionException e) {
+            e.printStackTrace();
+        } catch (ServerMissingSomethingException e) {
+            e.printStackTrace();
+        } catch (ServerDisabledException e) {
+            e.printStackTrace();
+        } catch (BadServerResponseException e) {
+            e.printStackTrace();
+        } catch (LaunchException e) {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
